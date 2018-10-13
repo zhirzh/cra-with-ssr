f81ff21e04fbf00cc9be227180e7ea2ba091237e
Shirsh Zibbu <shirsh.zibbu@innoplexus.com>
2018-10-14 02:04:13 +0530

    add react-renderer

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var { BUILD_DIR, PUBLIC_DIR } = require('./paths');
+var reactRenderer = require('./react-renderer');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,11 +19,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.get('/', reactRenderer);
+
 app.use(express.static(BUILD_DIR));
 app.use(express.static(PUBLIC_DIR));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/react-renderer.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var path = require('path');
+
+var React = require('react');
+var { renderToString } = require('react-dom/server');
+
+var { BUILD_DIR } = require('./paths');
+var App = require('../client/src/App');
+
+function reactRenderer(req, res) {
+  var app = renderToString(React.createElement(App));
+
+  var html = fs
+    .readFileSync(path.join(BUILD_DIR, 'index.html'), 'utf8')
+    .replace('__ROOT__', app);
+
+  return res.send(html);
+}
+
+module.exports = reactRenderer;
