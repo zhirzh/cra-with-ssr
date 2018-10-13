4c20ea4604f05fa905581ffeced1c72394036e1d
Shirsh Zibbu <shirsh.zibbu@innoplexus.com>
2018-10-14 02:30:01 +0530

    migrate server

--- server/paths.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-
-var BUILD_DIR = path.join(__dirname, '..', 'client', 'build');
-var PUBLIC_DIR = path.join(__dirname, 'public');
-
-module.exports = {
-  BUILD_DIR,
-  PUBLIC_DIR,
-};
