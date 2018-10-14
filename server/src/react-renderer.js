4d36f5650ca78be776245495c3122465a5b0d1cb
Shirsh Zibbu <shirsh.zibbu@innoplexus.com>
2018-10-14 21:16:32 +0530

    add redux to server/

--- server/src/react-renderer.js
+++ server/src/react-renderer.js
@@ -3,12 +3,20 @@ var path = require('path');
 
 var React = require('react');
 var { renderToString } = require('react-dom/server');
+var { Provider } = require('react-redux');
 
 var { BUILD_DIR } = require('./paths');
 var App = require('../../client/lib/App').default;
+var configureStore = require('../../client/lib/modules/store').default;
 
 function reactRenderer(req, res) {
-  var app = renderToString(<App />);
+  var store = configureStore();
+
+  var app = renderToString(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 
   var html = fs
     .readFileSync(path.join(BUILD_DIR, 'index.html'), 'utf8')
