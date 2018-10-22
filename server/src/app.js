01170880dfa0fc1563d90c1862f512394a2a83a0
Shirsh Zibbu <shirsh.zibbu@innoplexus.com>
2018-10-22 18:58:01 +0530

    remove cruft from server/

--- server/src/app.js
+++ server/src/app.js
@@ -1,24 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var { BUILD_DIR, PUBLIC_DIR } = require('./paths');
 var reactRenderer = require('./react-renderer');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use(reactRenderer);
 
@@ -36,9 +25,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  throw err;
 });
 
 module.exports = app;
